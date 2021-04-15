57a0f0ec730c2db7e0ee066db776b796c57e0d0f
dsmith <darrensmith223@comcast.net>
2021-04-14 20:28:26 -0400

    Add testing

--- test/test_inboxtracker.py
+++ test/test_inboxtracker.py
@@ -4,13 +4,14 @@ import inboxtracker
 
 @responses.activate
 def test_GetCampaignById():
-    campaignId = "test-campaign"
     responses.add(
         responses.GET,
-        "http://api.edatasource.com/v4/inbox/campaigns/test-campaign?Authorization=fake-key",
+        "http://api.edatasource.com/v4/inbox/campaigns/test-campaign",
         status=200,
         content_type='application/json',
         body='{"results": "yay"}'
     )
     apiKey = "fake-key"
+    campaignId = "test-campaign"
     response = inboxtracker.GetCampaignById(apiKey, campaignId)
+    assert response.status_code == 200
